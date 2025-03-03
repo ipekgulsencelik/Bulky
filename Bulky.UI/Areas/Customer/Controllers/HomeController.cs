80c1e04baf19daa49c6b49a811ae6e9eab405f5c
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-03-03 20:09:26 +0300

    Home and Details Page

--- Bulky.UI/Areas/Customer/Controllers/HomeController.cs
+++ Bulky.UI/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-using System.Diagnostics;
+using Bulky.DataAccess.Repository.IRepositories;
+using Bulky.Entity.Entities;
 using Bulky.UI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Bulky.UI.Areas.Customer.Controllers
 {
@@ -8,15 +11,24 @@ namespace Bulky.UI.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+             IEnumerable<Product> productList = await _unitOfWork.Products.GetAllAsync(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public async Task<IActionResult> Details(int productId)
+        {
+            Product product = await _unitOfWork.Products.GetAsync(x => x.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
